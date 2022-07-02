ecd51b08a215d7e4e112d23e84db6e201a9c2854
Chuck LU <chuck.lu@edenred.com>
2022-07-02 14:02:55 +0800

    initial version

---
+++ T372965/ViewModels/ViewModel1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using DevExpress.Mvvm;
+using System.Threading.Tasks;
+
+namespace T372965.ViewModels {
+    public class ViewModel1 : ViewModelBase {
+        public AsyncCommand AsyncCommand { get; set; }
+        public ViewModel1() {
+            AsyncCommand = new AsyncCommand(Calculate);
+        }
+        private async Task Calculate() {
+            try {
+                await somethingAsync();
+            } catch (Exception e) {
+                Console.WriteLine(e.Message);
+            }
+        }
+        private Task somethingAsync() {
+            throw new InvalidOperationException();
+        }
+    }
+}
